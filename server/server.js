b27ee91e6a64e78b3d4ab1bb47e6dbe4f777827a
Roshan <dearjha2019@gmail.com>
2026-05-30 10:39:17 +0530

    Updated

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const server = http.createServer(app);
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "https://any-one-talk.vercel.app/",
+  "https://any-one-talk.vercel.app",
 ];
 
 const io = new Server(server, {
